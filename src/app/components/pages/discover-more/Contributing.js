d953afba86498de6eda0304a3df274a33a827eba
Steve Fan <cia48621793@gmail.com>
2016-12-16 21:10:12 +0800

    init

---
+++ src/app/components/pages/discover-more/Contributing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from 'react-title-component';
+//import contributingText from '../../../../../../CONTRIBUTING.md';
+import MarkdownElement from '../../MarkdownElement';
+
+const contributingText = "TODO"
+
+const Contributing = () => (
+  <div>
+    <Title render={(previousTitle) => `Contributing - ${previousTitle}`} />
+    <MarkdownElement text={contributingText} />
+  </div>
+);
+
+export default Contributing;
